ae6a93d3599f1c549fedcefd57eb8e8b4ba6d411
Ilya <ilya.kochetov@gmail.com>
2017-05-04 23:41:10 +0300

    working buttons and mode select

--- TempControlV2.ino
+++ TempControlV2.ino
@@ -4,16 +4,16 @@
     Использует Arduino R3, DS18b20 (лучше водозащищенный), реле,
     LCD shield
 */
-#define   RELAY1          3  //порт реле
+#define   RELAY1          3   //порт реле
 #define   ONE_WIRE_BUS    11  //на каком пине датчик температуры
-#define   ONE_WIRE_PWR    12  //на каком пине датчик температуры
-#define   BUTTON1         4  //пин кнопки
+#define   ONE_WIRE_PWR    12  //на каком пине питание датчика температуры (используем internal pull-up)
+#define   A_BUTTON        0   //аналоговый пин кнопки
 /*
     Указываем, к каким пинам Arduino подключены выводы дисплея:
      RS, E, DB4, DB5, DB6, DB7
 */
-#define   LCD_RS          8 //(Data or Signal Display Selection)
-#define   LCD_E           9 //Enable
+#define   LCD_RS          8   //(Data or Signal Display Selection)
+#define   LCD_E           9   //Enable
 #define   LCD_BACK        10  //LCD backlit
 #define   LCD_DB4          4
 #define   LCD_DB5          5
@@ -24,6 +24,15 @@
 #define   CODE_VERSION  3     //версия программы
 #define   serial_speed  9600  //скорость сериального выхода
 #define   CYCLE_SPEED   1000  //задержка цикла
+
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
 
 #include  <Logging.h>         //библиотека для удобной работы с сериальным выводом
 #define   LOGLEVEL LOG_LEVEL_DEBUG    //NOOUTPUT,ERRORS,INFOS,DEBUG,VERBOSE
@@ -46,25 +55,29 @@ DeviceAddress mainSensor;     //адрес датчика температуры
 */
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
 
-
 //температурные константы
-#define TARGET_TEMP       26 //сколько хотим держать по-умолчанию
-#define TARGET_TEMP_MAX   66 //максимум для целевой температуры (актуально для пластиковых труб, чтобы не было больше 70)
-#define TARGET_TEMP_MIN   6 //сколько хотим держать по-умолчанию (сколько можно поставить по-миниумуму)
-#define TARGET_TEMP_STEP  2 //шаг изменения при нажатии кнопки
+#define TARGET_TEMP       25  //сколько хотим держать по-умолчанию
+#define TARGET_TEMP_MAX   65  //максимум для целевой температуры (актуально для пластиковых труб, чтобы не было больше 70)
+#define TARGET_TEMP_MIN   5   //сколько хотим держать по-умолчанию (сколько можно поставить по-миниумуму)
+#define TARGET_TEMP_STEP  1   //шаг изменения при нажатии кнопки
+#define TARGET_TEMP_JUMP  10  //шаг изменения при нажатии кнопки
 
-#define TEMP_DELTA   2  //на сколько можем охладится прежде чем включать заново
+#define TEMP_DELTA        2  //на сколько можем охладится прежде чем включать заново
 
 //статусы прибора
-#define STATE_OFF    0  //покой
-#define STATE_UP     1  //греем
-#define STATE_DOWN   2  //охлаждаем
+#define STATE_OFF         0  //покой
+#define STATE_UP          1  //греем
+#define STATE_DOWN        2  //охлаждаем
 
-float temp1_real;       //для реальной темпертуры сенсора
-int temp1;              //она же в виде int
+#define MODE_TARGET       0   //режим выбора целевой темпертуры;
 
-int target_temp;        //целевая температура
-int relay1_state;       //статус прибора
+float temp1_real        = 0;  //для реальной темпертуры сенсора
+int temp1               = 0;  //она же в виде int
+
+int target_temp         = TARGET_TEMP;   //целевая температура
+int relay1_state        = STATE_OFF;    //статус прибора
+int select_mode         = MODE_TARGET;  //что мы выбираем кнопками
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -87,21 +100,19 @@ void setup() {
 
   //инициализуем реле
   //pinMode(RELAY1, OUTPUT);
-  relay1_state = STATE_OFF;
-
-  //инициализуем кнопку
-  //pinMode(BUTTON1, INPUT_PULLUP);
-
-  target_temp = TARGET_TEMP;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  bool buttonState = false;//digitalRead(BUTTON1);
-  if (buttonState)  {
-    target_temp += TARGET_TEMP_STEP;
-    target_temp = (target_temp > TARGET_TEMP_MAX) ? TARGET_TEMP_MIN : target_temp;
+  lcd_key = read_LCD_buttons();  //читаем статус кнопок шилда
+  if (lcd_key == btnSELECT) {
+     select_mode   =  selectMode(select_mode);  //выбор вида изменений
+     showSelectMode(select_mode);
+     return;
+  }
+  if (select_mode == MODE_TARGET) {
+     target_temp    = selectTargetTemp(lcd_key, target_temp);
   }
 
   delay(CYCLE_SPEED);
@@ -145,22 +156,32 @@ void loop() {
 }
 
 /*
+ * показ режима выбора
+ */
+void showSelectMode(int select_mode)  {
+  lcd.setCursor(0, 0);
+  lcd.print("Select target   ");
+  lcd.setCursor(0, 1);
+  lcd.print("temperature     ");  
+}
+
+/*
    показ статусной строки
 */
 void showStatus(float target, int state) {
   lcd.setCursor(0, 1);
-  lcd.print("TARG:"); //+3 = 3
-  lcd.print((int)target);  //+4 = 7
-  lcd.print("C");    //+1 = 8
-  lcd.print(" ST:");   //+4 = 12
+  lcd.print("TARG:");         //+5 = 5
+  lcd.print((int)target);     //+2 = 7
+  lcd.print("C");             //+1 = 8
+  lcd.print(" ST:");          //+4 = 12
   if (state == STATE_OFF)  {
-    lcd.print("OFF "); //+3 = 15
+    lcd.print("OFF ");        //+4 = 16
   } else if (state == STATE_DOWN) {
-    lcd.print("COOL"); //4+ = 16
+    lcd.print("COOL");        //4+ = 16
   } else if (state == STATE_UP) {
-    lcd.print("HEAT"); //4+ = 16
+    lcd.print("HEAT");        //4+ = 16
   } else  {
-    lcd.print("???"); //4+ = 16
+    lcd.print("????");        //4+ = 16
   }
 }
 
@@ -171,11 +192,11 @@ void showTemp(float temp) {
   lcd.setCursor(0, 0);  //верхняя строка
   //ds18b20 вернет -127 при обрыве или другой ошибке
   if (temp != -127) {
-    lcd.print("TEMP:");  //+8
-    lcd.print(temp);        //+4 = 12
-    lcd.print("C");         //+1 = 13
+    lcd.print("TEMP:");     //+5
+    lcd.print(temp);        //+5 = 10
+    lcd.print("C     ");    //+6 = 16
   } else  {
-    lcd.print("TEMP SENSOR FAIL");
+    lcd.print("TEMP SENSOR FAIL");  //16
   }
 }
 
@@ -198,14 +219,41 @@ float getTemp(DeviceAddress sensorAddress) {
 }
 
 int relayStart(int relay) {
-  Log.Info("Starting relay %d"CR, relay);
   digitalWrite(relay, HIGH);
   return STATE_UP;
 }
 
 int relayStop(int relay) {
-  Log.Info("Stopping relay %d"CR, relay);
   digitalWrite(relay, LOW);
   return STATE_DOWN;
 }
 
+int selectMode(int mode) {
+  Log.Info("ONLY ONE MODE CURENTLY SUPPORTED. COME BACK LATER");
+  if (mode == MODE_TARGET) {
+  }
+  return mode;
+}
+
+int selectTargetTemp(int key, int temp) {
+    if (key == btnUP)     temp += TARGET_TEMP_STEP;
+    if (key == btnDOWN)   temp -= TARGET_TEMP_STEP;
+    if (key == btnLEFT)   temp -= TARGET_TEMP_JUMP;
+    if (key == btnRIGHT)  temp += TARGET_TEMP_JUMP;
+    temp = (temp > TARGET_TEMP_MAX) ? TARGET_TEMP_MAX : temp;
+    temp = (temp < TARGET_TEMP_MIN) ? TARGET_TEMP_MIN : temp;
+    return temp;
+}
+
+// read the buttons
+int read_LCD_buttons()  {
+  adc_key_in = analogRead(A_BUTTON);      // read the value from the sensor
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.1 us this threshold
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 250)  return btnUP;
+  if (adc_key_in < 450)  return btnDOWN;
+  if (adc_key_in < 650)  return btnLEFT;
+  if (adc_key_in < 850)  return btnSELECT;
+  return btnNONE;  // when all others fail, return this...
+}
